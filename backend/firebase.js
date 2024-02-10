f3587a752ca609bd89f3271fe41db44643a0df37
victoriaprisco <p.victoria@wustl.edu>
2024-02-10 15:06:45 -0600

    stack setup

---
+++ backend/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA1tJLgcwZAM-CpQycH9GF-3X50nJ0pSb8",
+  authDomain: "officehoursproject-e6c61.firebaseapp.com",
+  projectId: "officehoursproject-e6c61",
+  storageBucket: "officehoursproject-e6c61.appspot.com",
+  messagingSenderId: "499685175743",
+  appId: "1:499685175743:web:ded7be3089a7b0bdc7b10a",
+  measurementId: "G-SH0M4P2BFG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
