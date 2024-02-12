9ad0cc7525212a860c3f6a6a51966871dc9213af
sammya24 <89807548+sammya24@users.noreply.github.com>
2024-02-11 19:01:48 -0600

    changed default landing page to home, added home styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from "./Components/Login"
-import { BrowserRouter as Router} from 'react-router-dom';
-import {Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Home from "./Components/Home"
 
 function App() {
-  return (<>
-      <Router >
-        <Routes>
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </Router>
-    </>
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
