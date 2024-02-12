d2fd60414cadd239b16b37fe1088fb654629a3ed
sammya24 <89807548+sammya24@users.noreply.github.com>
2024-02-11 20:28:18 -0600

    added dashboard and auth.currentUser for accessing current user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Login from "./Components/Login"
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
 import Home from "./Components/Home"
+import Dashboard from './Components/Dashboard';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
