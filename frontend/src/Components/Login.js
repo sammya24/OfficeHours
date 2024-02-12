7d367bd2fb22d6a0a7f3c345f3523eca009747e0
victoriaprisco <p.victoria@wustl.edu>
2024-02-11 22:53:23 -0600

    forgot password functionality

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,28 +1,32 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
+import { useState, setState } from "react";
 
 const performLogin = (e) => {
     e.preventDefault()
     const email = e.target.email.value
-    const pass = e.target.password.value
+    const pass = e.target.current_password.value
     signInWithEmailAndPassword(auth, email, pass).then((credentials)=>{
-        console.log("Success!")
+        alert("Success!")
         setUser(credentials.user)
     }).catch((error)=>{
-        if(error.code == "auth/invalid-credential") {
+        if(error.code === "auth/invalid-credential") {
             console.log("User with that email was not found, creating new account with these credentials.")
             createUserWithEmailAndPassword(auth, email, pass).then((credentials)=>{
+                alert("New account created!")
                 console.log("Successfully created new account")
                 setUser(credentials.user)
             }).catch((error) => {
                 console.error(error.code)
             })
         }
-        else if (error.code == "auth/invalid-email") {
+        else if (error.code === "auth/invalid-email") {
             console.log("Invalid Email")
         }
     })
 }
+
+
 
 var user;
 const setUser = (u) => {
@@ -30,11 +34,25 @@ const setUser = (u) => {
     console.log(user)
 }
 const Login = () => {
+    const [email, setEmail] = useState("")
+    const handleForgotPassword = () => {
+        console.log("resetting password...")
+        sendPasswordResetEmail(auth, email).then(()=>{
+            alert("An email to reset your password has been sent!")
+        }).catch((e)=>{
+            if(e.code === "auth/invalid-email") {
+                alert("Please enter a valid email and then click \"forgot password?\"")
+            }
+        })
+    }
     return (<>
         <form onSubmit={performLogin}>
-            <input type="text" name="email"/>
-            <input type="text" name="password"/>
-            <input type="submit" />
+            <input type="text" name="email" onChange={(e)=>{
+                 setEmail(e.target.value)
+            }}/>
+            <input type="password" name="current_password"/>
+            <input type="submit" value="login"/>
+            <input type="button" value="forgot password?" onClick={handleForgotPassword} />
         </form>
     </>)
 }
